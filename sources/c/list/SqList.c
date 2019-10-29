caec7269bbaa2e1d831952df02fce439387173f6
ruyuejun <99316632@qq.com>
2019-10-29 19:23:45 +0800

    顺序表C语言版完成

--- sources/c/list/SqList.c
+++ sources/c/list/SqList.c
@@ -18,11 +18,11 @@ typedef struct SqList{
     int         size;           // 当前分配的存储容量，以 sizeof(ElemType)为单位
 }SqList;
 
-// 构造一个空的顺序表L
+// 构造空顺序表
 void SqListInit(SqList *L) {
     L->elem = (ElemType *)malloc(LIST_MAX_SIZE * sizeof(ElemType));
     if (!L->elem)  {            // 存储分配失败
-        printf("内存分配失败");
+        printf("内存分配失败\n");
         exit(ERROR);         
     }
     L->length = 0;
@@ -30,55 +30,161 @@ void SqListInit(SqList *L) {
 }
 
 // 获取线性表长度
-int SqListLength(SqList *L) {
-    return L->length;
-}
-
-// 显示
-void SqListDisplay(SqList *L) {
-    if (L->length == 0) {
-        printf("数据结构无长度");
-        exit(UNEXEC);
-    }
-    int i;
-    for (i = 0; i < L->length; i++) {
-        prinft("item[%d]=%d\n", i, L->elem[i]);
-    }
+int SqListLength(SqList L) {
+    return L.length;
 }
 
 // 插入
-void SqListInsert(SqList *L, int index, ElemType e) {
-    int i ;
-    for (i = 0; i < L->length; i++) {
-        if
+Status SqListInsert(SqList *L, int index, ElemType e) {
+
+    // 顺序表为空，但是插入位置在第二位
+    if (L->length == 0 && index != 1) {
+        printf("索引不合法\n");
+        return ERROR;
     }
+
+    // 顺序表为空，插入位置正确
+    if (L->length == 0 && index == 1) {
+        L->elem[0] = e;
+        L->length++;
+        return TRUE;
+    }
+
+    // 索引越界
+    if (index < 1 || index > L->length + 1) {
+        printf("索引不合法\n");
+        return ERROR;
+    }
+
+    // 存储空间已满，增加分配
+    ElemType *newbase;      // 增加分配后的新地址
+    if (L->length >= L->size) {
+   
+        if ( !(newbase = (ElemType *)realloc(L->elem, (L->size + LIST_INCREMENT) * sizeof(ElemType))) ) {
+            printf("内存分配失败\n");
+            exit(ERROR); 
+        }
+
+        L->elem = newbase;
+        L->size += LIST_INCREMENT;
+    }
+
+    // 插入
+    int lastIndex;      // 最后一个元素的索引的后一位
+    for (lastIndex =  L->length; lastIndex > index; lastIndex--) {
+        L->elem[lastIndex] = L->elem[lastIndex - 1];
+    }
+    L->elem[index] = e;
+    L->length++;
+    return TRUE;
 }
 
 // 删除  按照位序删除
+Status SqListDelete(SqList *L, int index) {
+    
+    // 顺序表为空
+    if (L->length == 0) {
+        printf("顺序表为空\n");
+        return ERROR;
+    }
+
+    // 删除
+    int i;
+    for (i = index; i < L->length - 1; i++) {
+        L->elem[index] = L->elem[index + 1];
+    }
+    L->length--;
+    return TRUE;
+}
 
 // 修改  按照位序修改
+Status SqListUpdate(SqList *L, int index, ElemType e) {
+    
+    if (index < 1 || index > L->length) {
+        printf("索引位置不正确");
+        return ERROR;
+    }
+    
+    L->elem[index] = e;
+    return TRUE;
+}
 
 // 查询  按照位序查询值
 ElemType SqListGetElem(SqList *L, int index) {
     if (index < 1 || index > L->length) {
-        printf("索引越界");
+        printf("索引越界\n");
         exit(ERROR);
     }
     return L->elem[index];
 }
 
-// 查询  按照值查询位序
-int SqListLocateElem(SqList *L,){
+
+// 查询前驱
+Status SqListPriorElem(SqList *L, ElemType e, ElemType *pe){
+
+    if (L->length <= 1) {
+        printf("顺序表元素不足，无法查询\n");
+        return UNEXEC;
+    }
+
+    if (L->elem[0] == e) {
+        printf("第一个元素无前驱\n");
+        return NOTFOUND;
+    }
+
+    // 查找 e 所在位置
+    int i;
+    int index = 0;
+    for (i = 1; i < L->length; i++) {
+        if (L->elem[i] == e) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == 0) {
+        printf("未找到该元素\n");
+        return NOTFOUND;
+    }
+
+    *pe = L->elem[index - 1];
+    return TRUE;
 
 }
 
 
-// 查询前驱
-// ElemType Pro
-
-
 // 查询后继
+Status SqListNextElem(SqList *L, ElemType e, ElemType *ne){
 
+    if (L->length <= 1) {
+        printf("顺序表元素不足，无法查询\n");
+        return UNEXEC;
+    }
+
+    if (L->elem[L->length + 1] == e) {
+        printf("最后一个元素无后继\n");
+        return NOTFOUND;
+    }
+
+    // 查找 e 所在位置
+    int i;
+    int index = 0;
+    for (i = 1; i < L->length; i++) {
+        if (L->elem[i] == e) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == 0) {
+        printf("未找到该元素\n");
+        return NOTFOUND;
+    }
+
+    *ne = L->elem[index + 1];
+    return TRUE;
+
+}
 
 
 // 销毁
@@ -93,12 +199,3 @@ void SqListDestroy(SqList *L) {
 void ClearList(SqList *L) {
     L->length = 0;
 }
-
-// 判断 是否是空表
-Status SqListEmpty(SqList *L) {
-    if (L->length == 0) {
-        return TRUE;
-    }
-    return FALSE;
-}
-
--- sources/c/main.c
+++ sources/c/main.c
@@ -5,8 +5,6 @@ void test_SqList();             // 测试顺序表
 
 int main() {
 
-
-
     test_SqList();
 
     return 0;
@@ -16,7 +14,14 @@ void test_SqList() {
     printf("********** 顺序表测试开始 **********\n");
     struct SqList L;
     SqListInit(&L);
-    SqListDisplay(&L);
-    printf("%d\n", L.length);
+    printf("长度 %d\n", SqListLength(L));
+    SqListInsert(&L, 1, 17);
+    SqListInsert(&L, 2, 18);
+    SqListInsert(&L, 3, 19);
+    printf("长度 %d\n", SqListLength(L));
+    SqListUpdate(&L, 2, 22);
+    printf("修改 %d\n", L.elem[2]);
+    printf("删除 %d\n", SqListDelete(&L, 2));
+    printf("长度 %d\n", SqListLength(L));
     printf("********** 顺序表测试结束 **********\n");
 }
