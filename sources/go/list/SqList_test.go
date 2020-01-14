f9acf60dce3b1b806a52a5ab33dab57caf0421f4
ruyuejun <99316632@qq.com>
2020-01-14 17:16:00 +0800

    顺序表添加单元测试

--- sources/go/list/SqList_test.go
+++ sources/go/list/SqList_test.go
@@ -1,1 +1,26 @@
 package list
+
+import "testing"
+
+func TestSqList_PushBack(t *testing.T) {
+	l := NewSqList()
+	l.PushBack(11)
+	l.PushBack(12)
+	l.PushBack(13)
+	l.PushBack(14)
+	l.PushBack(15)
+	l.PushBack(16)
+	t.Log(l.items)
+}
+
+func TestSqList_Insert(t *testing.T) {
+	l := NewSqList()
+	l.Insert(1,11)
+	l.Insert(5,12)
+	l.Insert(2,13)
+	l.Insert(3,14)
+	l.Insert(2,15)
+	l.Insert(2,16)
+	l.Insert(2,17)
+	t.Log(l.items)
+}
