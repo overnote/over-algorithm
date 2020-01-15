25a582ad2a9893de372b3edf2a6256acc0641ec5
yaya <2868171889@qq.com>
2020-01-15 23:01:39 +0800

    删除js算法文件

--- sources/js/index.js
+++
@@ -1,7 +0,0 @@
-import TestSqList from "./test/Test_SqList"
-import TestLinkList from "./test/Test_LinkList"
-import TestDoubleList from "./test/Test_DoubleList"
-
-// TestSqList()            // 测试顺序表
-// TestLinkList()              // 测试单链表
-TestDoubleList()            // 测试双向循环链表
