c16e3d27436a8a47ca65f750a1a85cc6c4a42405
ruyuejun <99316632@qq.com>
2021-05-05 22:53:14 +0800

    新增:vscode配置,C测试文件

--- sources/c/list/CircleList.c
+++ sources/c/list/CircleList.c
@@ -1,13 +1,13 @@
 /**
- * 单链表
+ * 循环链表
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "LinkList.h"
+#include "CircleList.h"
 
 // 创建结点
-Node* newNode(DataType data){
+Node* newNode(DataType data) {
     Node *node = malloc(sizeof(Node));
     if(node == NULL){
         printf("申请结点内存失败\n");
@@ -18,51 +18,51 @@ Node* newNode(DataType data){
     return node;
 }
 
-// 创建单链表
-LinkList* newLinkList(){
-    // 创建头结点，返回头指针
+// 创建循环链表
+CircleList* newCircleList(){
+    // 创建头节点，返回头指针
     Node *head = newNode(0);
+    head->next = head;  // 循环链表！！！！！
 
-    LinkList *L = malloc(sizeof(LinkList));
+    CircleList *L = malloc(sizeof(CircleList));
     if(L == NULL){
         printf("申请单链表内存失败\n");
         exit(1);
     }
-    
+
     L->head = head;
     L->length = 0;
     return L;
 }
 
 // 增：插入结点
-// 约定：带头结点的链表，插入时，只能在头结点之后插入，也不允许插入超过最大元素个数的位置
-int insert(LinkList *L, DataType e, int index){
-
+// 约定：插入时，只能在头结点之后插入，也不允许插入超过最大元素个数的位置
+int insert(CircleList *L, DataType e, int index){
     if(index < 1 || index > L->length + 1){
-        printf("插入位置非法\n");
+        printf("插入位置不合法\n");
         return -1;
     }
 
     // 找到插入位置前一个位置：也可以使用 locate函数
     Node *p = L->head;
     int k = 0;
-    while(p->next != NULL && k < index - 1){
+    while(p->next != L->head && k < index - 1){
         p = p->next;
         k++;
     }
 
-    // 创建要插入的结点
+    // 创建要插入的节点
     Node *q = newNode(e);
     q->next = p->next;
     p->next = q;
 
-    L->length++;  // 不要忘记存储的长度+1
+    L->length++;
     return 1;
 }
 
 // 删：根据位置删除，返回被删除的元素
-int delete(LinkList *L, int index, DataType *e){
-
+int delete(CircleList *L, int index, DataType *e){
+    
     if(index < 1 || index > L->length){
         printf("删除位置非法\n");
         return -1;
@@ -71,7 +71,7 @@ int delete(LinkList *L, int index, DataType *e){
     // 找到删除位置前一个位置：也可以使用 locate函数
     Node *p = L->head;
     int k = 0;
-    while(p->next != NULL && k < index - 1){
+    while(p->next != L->head && k < index - 1){
         p = p->next;
         k++;
     }
@@ -87,9 +87,7 @@ int delete(LinkList *L, int index, DataType *e){
 }
 
 // 改
-void update(LinkList *L, int index, DataType e){
-
-    // 找到修改位置：这里使用 locate 函数
+void update(CircleList *L, int index, DataType e){
     Node *p = locate(L, index);
     if(p == NULL){
         return;
@@ -98,9 +96,9 @@ void update(LinkList *L, int index, DataType e){
 }
 
 // 查：根据值查询结点地址
-Node* search(LinkList *L, DataType e){
+Node* search(CircleList *L, DataType e){
     Node *p = L->head;
-    while(p->next != NULL){
+    while(p->next != L->head){
         if(p->data == e){
             return p;
         }
@@ -109,40 +107,41 @@ Node* search(LinkList *L, DataType e){
     return NULL;
 }
 
-// 查：根据位置查询结点地址
-Node* locate(LinkList *L, int index){
-    if(index < 0 || index > L->length + 1 ){
+// 定位
+Node* locate(CircleList *L, int index){
+    if(index < 0 || index > L->length + 1){
         printf("获取位置不合法\n");
         return NULL;
     }
 
     Node *p = L->head;
     int k = 0;
-    while(p->next != NULL && k < index){
+    while(p->next != L->head && k < index){
         p = p->next;
         k++;
     }
     return p;
 }
 
-// 获取表长度：若未保存该值，则可以通过循环获得
-int length(LinkList *L){
+// 获取表长度:没有头结点一般使用循环获取长度
+int length(CircleList *L){
     return L->length;
 }
 
 // 清空表:仅保留头结点
-void clear(LinkList *L){
+void clear(CircleList *L){
     Node *p = L->head;
-    while(p->next != NULL){
+    while(p->next != L->head){
         Node *q = p->next;
         p->next = q->next;
         free(q);
     }
+    L->head->next = L->head;    // 重新指定头结点循环
     L->length = 0;
 }
 
 // 销毁表
-void destroy(LinkList *L){
+void destroy(CircleList *L){
     Node *p = L->head;
     while(p->next != NULL){
         Node *q = p->next;
@@ -152,8 +151,8 @@ void destroy(LinkList *L){
     free(L);
 }
 
-// 显示单链表
-void display(LinkList *L){
+// 显示循环链表
+void display(CircleList *L){
     if(L->length == 0){
         printf("空链表\n");
         return;
@@ -161,9 +160,9 @@ void display(LinkList *L){
 
     Node *p = L->head;
     int pos = 0;
-    while(p != NULL){
-        if(pos == L->length){    // 最后一位
-            printf("%d\n", p->data);
+    while(p->next != NULL){
+        if(pos == L->length){
+            printf("%d->%d->...\n", p->data, L->head->data);
             break;
         }
         printf("%d->", p->data);
